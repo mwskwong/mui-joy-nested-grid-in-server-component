d4b509a8dd4c2d61dd5814d9a9a6647b34b413e5
Matthew Kwong <matthewkwong2@gmail.com>
2023-07-12 08:46:24 +0800

    feat: demonstrate the issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,34 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { Box, Grid, Sheet } from "@mui/joy";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2} columns={24}>
+        <Grid xs={8}>
+          <Sheet>xs=8/24</Sheet>
+        </Grid>
+        <Grid container xs={16}>
+          <Grid xs={12}>
+            <Sheet>nested xs=12/24</Sheet>
+          </Grid>
+          <Grid xs={12}>
+            <Sheet>nested xs=12/24</Sheet>
+          </Grid>
+        </Grid>
+        <Grid xs={8}>
+          <Sheet>xs=8/24</Sheet>
+        </Grid>
+        <Grid container xs={16} columns={12}>
+          <Grid xs={6}>
+            <Sheet>nested xs=6/12</Sheet>
+          </Grid>
+          <Grid xs={6}>
+            <Sheet>nested xs=6/12</Sheet>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
